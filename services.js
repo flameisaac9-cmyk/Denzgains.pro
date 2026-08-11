9cfc0c58bfbbbfdf2aec01fcceca744f5339ae47
flameisaac9-cmyk <flameisaac9@gmail.com>
2026-08-11 09:22:46 +0300

    Add services data for social media platforms

---
+++ services.js
@@ -0,0 +1,85 @@
+const SERVICES_DATA = [
+  {
+    id: "instagram-followers",
+    platform: "instagram",
+    name: "Instagram Followers",
+    pricePer1K: 150,
+    min: 100,
+    max: 100000
+  },
+  {
+    id: "instagram-likes",
+    platform: "instagram",
+    name: "Instagram Likes",
+    pricePer1K: 80,
+    min: 100,
+    max: 100000
+  },
+  {
+    id: "instagram-views",
+    platform: "instagram",
+    name: "Instagram Views",
+    pricePer1K: 40,
+    min: 100,
+    max: 500000
+  },
+
+  {
+    id: "tiktok-followers",
+    platform: "tiktok",
+    name: "TikTok Followers",
+    pricePer1K: 180,
+    min: 100,
+    max: 100000
+  },
+  {
+    id: "tiktok-likes",
+    platform: "tiktok",
+    name: "TikTok Likes",
+    pricePer1K: 70,
+    min: 100,
+    max: 100000
+  },
+  {
+    id: "tiktok-views",
+    platform: "tiktok",
+    name: "TikTok Views",
+    pricePer1K: 35,
+    min: 100,
+    max: 500000
+  },
+
+  {
+    id: "youtube-subscribers",
+    platform: "youtube",
+    name: "YouTube Subscribers",
+    pricePer1K: 300,
+    min: 100,
+    max: 50000
+  },
+  {
+    id: "youtube-views",
+    platform: "youtube",
+    name: "YouTube Views",
+    pricePer1K: 100,
+    min: 100,
+    max: 500000
+  },
+
+  {
+    id: "facebook-followers",
+    platform: "facebook",
+    name: "Facebook Followers",
+    pricePer1K: 150,
+    min: 100,
+    max: 100000
+  },
+  {
+    id: "facebook-likes",
+    platform: "facebook",
+    name: "Facebook Likes",
+    pricePer1K: 80,
+    min: 100,
+    max: 100000
+  }
+];
